24609aa7a996b23abc74fbaaeacbfb69a739d6d0
vaibhoo17 <vaibhavb479@gmail.com>
2022-03-04 11:36:57 +0530

    First commit

---
+++ Demo1/src/com/vaibhoo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.vaibhoo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Vadapav");
+	}
+
+}
